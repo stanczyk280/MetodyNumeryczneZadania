c5090c06f7c6a1487e859c5643aa6d509bf89d54
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2023-01-06 16:09:47 +0100

    Całki

---
+++ Calki/Calka.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calki
+{
+    public static class Calka
+    {
+        public delegate double OneArgFunc(double x);
+
+        public delegate double TwoArgFunc(double x, double y);
+
+        public static double MetodaProstokatowLeftPoint(OneArgFunc Func, double xa, double xb, int n)
+        {
+            double przedzial = xb - xa;
+            double h = przedzial / n;
+            double sum = 0;
+            for (var i = 0; i <= n - 1; i++)
+            {
+                sum += (Func(xa + i * h));
+            }
+            sum = h * sum;
+            return sum;
+        }
+
+        public static double MetodaProstokatowRightPoint(OneArgFunc Func, double xa, double xb, int n)
+        {
+            double przedzial = xb - xa;
+            double h = przedzial / n;
+            double sum = 0;
+            for (var i = 0; i <= n - 1; i++)
+            {
+                sum += (Func(xb - i * h));
+            }
+            sum = h * sum;
+            return sum;
+        }
+
+        public static double MetodaProstokatowMidPoint(OneArgFunc Func, double xa, double xb, int n)
+        {
+            double przedzial = xb - xa;
+            double h = przedzial / n;
+            double sum = 0;
+            for (var i = 0; i <= n - 1; i++)
+            {
+                sum += Func(xa + (h / 2 + (i * h)));
+            }
+            sum = h * sum;
+            return sum;
+        }
+
+        public static double MetodaTrapezow(OneArgFunc Func, double xa, double xb, int n)
+        {
+            double h = (xb - xa) / n;
+            //double alfa = (Func(xa) * xb - Func(xb) * xa) / (xb - xa);
+            //double beta = (Func(xb) - Func(xa)) / (xb - xa);
+            double sum = 0;
+            for (var i = 0; i <= n - 1; i++)
+            {
+                sum += 2 * Func(xa + i * h);
+            }
+            sum = (h / 2) * sum;
+            return sum;
+        }
+
+        public static double MetodaSimpsona(OneArgFunc Func, double xa, double xb, int n)
+        {
+            double przedzial = xb - xa;
+            double h = przedzial / n;
+            double sum = 0;
+            for (var i = 0; i < n - 1; i++)
+            {
+                sum += Func(xa + i * h) + 4 * Func(xa + i * h) + Func(xa + (i + 1) * h);
+            }
+            sum = (h / 6) * sum;
+            return sum;
+        }
+    }
+
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            int dokladnosc = 1000;
+            Console.WriteLine("Dokladnosc calkowania: " + dokladnosc);
+            Console.WriteLine("===============================================================");
+            Console.WriteLine("Metoda prostokatow:");
+            Console.WriteLine();
+            Console.WriteLine("Metoda prostokatow left point:");
+            Console.WriteLine(Calka.MetodaProstokatowLeftPoint(Funkcje.Custom3, -1, 1, dokladnosc));
+            Console.WriteLine("Metoda prostokatow right point:");
+            Console.WriteLine(Calka.MetodaProstokatowRightPoint(Funkcje.Custom3, -1, 1, dokladnosc));
+            Console.WriteLine("Metoda prostokatow mid point:");
+            Console.WriteLine(Calka.MetodaProstokatowMidPoint(Funkcje.Custom3, -1, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda  trapezow:");
+            Console.WriteLine(Calka.MetodaTrapezow(Funkcje.Custom3, -1, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda  Simpsona:");
+            Console.WriteLine(Calka.MetodaSimpsona(Funkcje.Custom3, -1, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda prostokatow:");
+            Console.WriteLine();
+            Console.WriteLine("Metoda prostokatow left point:");
+            Console.WriteLine(Calka.MetodaProstokatowLeftPoint(Funkcje.Custom4, 0, 1, dokladnosc));
+            Console.WriteLine("Metoda prostokatow right point:");
+            Console.WriteLine(Calka.MetodaProstokatowRightPoint(Funkcje.Custom4, 0, 1, dokladnosc));
+            Console.WriteLine("Metoda prostokatow mid point:");
+            Console.WriteLine(Calka.MetodaProstokatowMidPoint(Funkcje.Custom4, 0, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda  trapezow:");
+            Console.WriteLine(Calka.MetodaTrapezow(Funkcje.Custom4, 0, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda  Simpsona:");
+            Console.WriteLine(Calka.MetodaSimpsona(Funkcje.Custom4, 0, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda prostokatow:");
+            Console.WriteLine();
+            Console.WriteLine("Metoda prostokatow left point:");
+            Console.WriteLine(Calka.MetodaProstokatowLeftPoint(Funkcje.Custom5, 0, 1, dokladnosc));
+            Console.WriteLine("Metoda prostokatow right point:");
+            Console.WriteLine(Calka.MetodaProstokatowRightPoint(Funkcje.Custom5, 0, 1, dokladnosc));
+            Console.WriteLine("Metoda prostokatow mid point:");
+            Console.WriteLine(Calka.MetodaProstokatowMidPoint(Funkcje.Custom5, 0, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda  trapezow:");
+            Console.WriteLine(Calka.MetodaTrapezow(Funkcje.Custom5, 0, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Metoda  Simpsona:");
+            Console.WriteLine(Calka.MetodaSimpsona(Funkcje.Custom5, 0, 1, dokladnosc));
+            Console.WriteLine();
+            Console.WriteLine("===============================================================");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Calki/Funkcje.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calki
+{
+    public static class Funkcje
+    {
+        public static double Sqrt(double x)
+            => Math.Sqrt(x);
+
+        // x kwadrat
+        public static double Square(double x)
+            => x * x;
+
+        //Sinus x
+        public static double Sinx(double x)
+            => Math.Sin(x);
+
+        //funkcja liniowa dwóch ziennych
+        public static double MultipleFunctionOfxy(double x, double y)
+            => x * y;
+
+        // dowolna funkcja dwóch zmiennych
+        public static double FunctiononOfx2y(double x, double y)
+            => x * x * y;
+
+        public static double Exp(double x)
+            => Math.Exp(x);
+
+        //f(x)=xsin(x^2)+1
+        public static double Custom1(double x)
+            => x * Math.Sin(x * x) + 1;
+
+        //f(x)=3x^3−2x^2+1
+        public static double Custom2(double x)
+            => 3 * x * x * x - 2 * x * x + 1;
+
+        public static double Custom3(double x)
+            => 4 * x * x * x + 5 * x * x + 1;
+
+        public static double Custom4(double x)
+            => (2 / (Math.Sqrt(Math.PI))) * Math.Exp(-(x * x));
+
+        public static double Custom5(double x)
+            => Math.Cos(x) + Math.Exp(x) + Math.Tan(x);
+    }
+}
