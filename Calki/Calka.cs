e55a1f0e8650bc29b730e8599bba1a7068f54813
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2023-01-06 21:37:16 +0100

    Monte-carlo

--- Calki/Calka.cs
+++ Calki/Calka.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -70,11 +71,33 @@ namespace Calki
             double przedzial = xb - xa;
             double h = przedzial / n;
             double sum = 0;
-            for (var i = 0; i < n - 1; i++)
+            for (var i = 0; i <= n - 1; i++)
             {
                 sum += Func(xa + i * h) + 4 * Func(xa + i * h) + Func(xa + (i + 1) * h);
             }
             sum = (h / 6) * sum;
+            return sum;
+        }
+
+        public static double GetDoubleRandom(double xa, double xb)
+        {
+            var random = new Random();
+            var doubleRandom = random.NextDouble();
+            var rangeDoubleRandom = doubleRandom * (xb - xa) + xa;
+            return rangeDoubleRandom;
+        }
+
+        public static double MetodaStohastyczna(OneArgFunc Func, double xa, double xb, int n)
+        {
+            Random rnd = new Random();
+            double przedzial = xb - xa;
+            double h = przedzial / n;
+            double sum = 0;
+            for (var i = 1; i <= n; i++)
+            {
+                sum += Func(GetDoubleRandom(xa, xb));
+            }
+            sum = h * sum;
             return sum;
         }
     }
@@ -149,6 +172,8 @@ namespace Calki
             Console.WriteLine();
             Console.WriteLine("===============================================================");
             Console.WriteLine();
+            Console.WriteLine("Metoda stohastyczna:");
+            Console.WriteLine(Calka.MetodaStohastyczna(Funkcje.Custom3, -1, 1, dokladnosc));
         }
     }
 }
